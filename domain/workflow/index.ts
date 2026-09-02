6b58cd8592c52ea9ebd1f499ac7f6bfb79c960c7
virkkomal1515-ctrl <virk.komal1515@gmail.com>
2026-09-02 21:08:22 +0530

    feat: export execution engine contracts

--- domain/workflow/index.ts
+++ domain/workflow/index.ts
@@ -1,13 +1,14 @@
 export type {
   ActionConfig, ActionNode, ActionOperation, ConditionConfig, ConditionNode,
-  ConditionOperator, EndConfig, EndNode, ExecutionLog, ExecutionResult,
-  ExecutionStatus, NodeConfig, NodeExecutionResult, NodeExecutionStatus,
+  ConditionOperator, EndConfig, EndNode, ExecutionFailure, ExecutionInput, ExecutionLog, ExecutionResult,
+  ExecutionSimulationOptions, ExecutionStatus, NodeConfig, NodeExecutionResult, NodeExecutionStatus,
   NodeType, NotificationConfig, NotificationNode, Position, SourcePort,
   TargetPort, TriggerConfig, TriggerNode, TriggerType, Workflow, WorkflowEdge,
   WorkflowNode, WorkflowStatus,
 } from "./types";
 export { fromPersistence, toPersistence } from "./transforms";
 export { addEdge, addNode, createDefaultNode, deleteNodes, moveNode, updateNodeConfig } from "./operations";
+export { executeWorkflow } from "./execution";
 export {
   isNodeConfigForType, validateEdge, validateEdges, validateForPublish,
   validateNode, validateWorkflow,
